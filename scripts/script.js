6f9b19cb2a77a258a08b077c8463d714ff7e6511
iXDiXDi <ixdixdi@gmail.com>
2023-01-13 23:22:17 +0500

    4th lesson homework

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,15 @@ document.getElementById('price-action').onclick = function () {
         alert('Спасибо за заявку, мы свяжемся с Вами в ближайшее время!')
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector('.price-image');
+    document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.5) / 10) + 'px,' + ((event.clientY * 0.5) / 10) + 'px,0px)';
+    });
+
+    const elem = document.querySelector(".main");
+    document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' - (0.5 * window.scrollY) + 'px';
+    })
+});
