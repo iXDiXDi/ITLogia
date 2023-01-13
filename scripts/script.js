ecf567285ed41c9b64ed44563f210dc8853102cd
iXDiXDi <ixdixdi@gmail.com>
2023-01-13 23:10:28 +0500

    4th lesson online

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+document.getElementById('main-action').onclick = function () {
+    document.getElementById('cars').scrollIntoView({behavior:'smooth'})
+}
+
+let buttons = document.getElementsByClassName('car-button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById('price').scrollIntoView({behavior:'smooth'})
+    }
+}
+
+document.getElementById('price-action').onclick = function () {
+    if (document.getElementById('name').value === '') {
+        alert('Заполните поле имя!')
+    } else if (document.getElementById('phone').value === '') {
+        alert('Заполните поле телефон!')
+    } else if (document.getElementById('car').value === '') {
+        alert('Заполните поле автомобиль!')
+    } else {
+        alert('Спасибо за заявку, мы свяжемся с Вами в ближайшее время!')
+    }
+}
